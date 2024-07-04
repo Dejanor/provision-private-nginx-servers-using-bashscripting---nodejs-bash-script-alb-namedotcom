c0bb309f280a9a398972047e67da52f9a5a2b637
Dejanor <dejanaemzy37@gmail.com>
2024-07-04 07:17:21 +0100

    Update deploy-node-app.sh

--- deploy-node-app.sh
+++ deploy-node-app.sh
@@ -1,26 +1,5 @@
-#!/usr/bin/bash
-sudo apt update
-sudo apt install nginx -y
-sudo apt install nodejs -y
-sudo apt install npm -y
-sudo ufw --force enable
-sudo ufw allow OpenSSH
-sudo ufw allow 'Nginx Full'
-cd /$(whoami)/ || cd /home/$(whoami)
-git clone https://github.com/Janortop5/altschool-december-nginx-challenge.git
-cd /$(whoami)/altschool-december-nginx-challenge || cd /home/$(whoami)/altschool-december-nginx-challenge
-mkdir node-app
-mv package.json server.js node-app/
-cd /$(whoami)/altschool-december-nginx-challenge/node-app || cd /home/$(whoami)/altschool-december-nginx-challenge/node-app
+#!/bin/bash
+cd /app
 npm install
-npm install -g pm2
-npm install uuid@latest
-pm2 start server.js
-pm2 startup
-mkdir /var/www/eaaladejana.live
-touch /var/www/eaaladejana.live/index.html
-cp /$(whoami)/altschool-december-nginx-challenge/eaaladejana.live /etc/nginx/sites-available/eaaladejana.live || cp /home/$(whoami)/altschool-december-nginx-challenge/eaaladejana.live /etc/nginx/sites-available/eaaladejana.live
-ln -s /etc/nginx/sites-available/eaaladejana.live /etc/nginx/sites-enabled/eaaladejana.live
-rm /etc/nginx/sites-enabled/default
-systemctl restart nginx
-pm2 list
+node server.js &
+nginx -g 'daemon off;'
