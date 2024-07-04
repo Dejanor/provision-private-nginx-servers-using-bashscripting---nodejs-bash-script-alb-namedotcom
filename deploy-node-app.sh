1982ac8d8270b49ed53a89c22fb88536d7af2db1
Dejanor <dejanaemzy37@gmail.com>
2024-07-04 06:44:31 +0100

    Update deploy-node-app.sh

--- deploy-node-app.sh
+++ deploy-node-app.sh
@@ -12,15 +12,15 @@ cd /$(whoami)/altschool-december-nginx-challenge || cd /home/$(whoami)/altschool
 mkdir node-app
 mv package.json server.js node-app/
 cd /$(whoami)/altschool-december-nginx-challenge/node-app || cd /home/$(whoami)/altschool-december-nginx-challenge/node-app
-sudo npm install
-sudo npm install -g pm2
-sudo npm install uuid@latest
-sudo pm2 start server.js
-sudo pm2 startup
-sudo mkdir /var/www/eaaladejana.live
-sudo touch /var/www/eaaladejana.live/index.html
-sudo cp /$(whoami)/altschool-december-nginx-challenge/eaaladejana.live /etc/nginx/sites-available/eaaladejana.live || sudo cp /home/$(whoami)/altschool-december-nginx-challenge/eaaladejana.live /etc/nginx/sites-available/eaaladejana.live
-sudo ln -s /etc/nginx/sites-available/eaaladejana.live /etc/nginx/sites-enabled/eaaladejana.live
-sudo rm /etc/nginx/sites-enabled/default
-sudo systemctl restart nginx
-sudo pm2 list
+npm install
+npm install -g pm2
+npm install uuid@latest
+pm2 start server.js
+pm2 startup
+mkdir /var/www/eaaladejana.live
+touch /var/www/eaaladejana.live/index.html
+cp /$(whoami)/altschool-december-nginx-challenge/eaaladejana.live /etc/nginx/sites-available/eaaladejana.live || sudo cp /home/$(whoami)/altschool-december-nginx-challenge/eaaladejana.live /etc/nginx/sites-available/eaaladejana.live
+ln -s /etc/nginx/sites-available/eaaladejana.live /etc/nginx/sites-enabled/eaaladejana.live
+rm /etc/nginx/sites-enabled/default
+systemctl restart nginx
+pm2 list
